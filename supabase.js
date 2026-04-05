2057f99a38c34cbebe8d1de43a7ffe98a8d7392f
*Drперc* <stepchik.tringer@gmail.com>
2026-04-05 14:15:56 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
 const SUPABASE_URL = 'https://mgalvfcnytusbklahkjy.supabase.co';
-const SUPABASE_KEY = 'твoй ключ';
+const SUPABASE_KEY = 'sb_publishable_GRAiUwNcIIDaBwl8Ux1TuA_1JhaIv6h';
 
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
