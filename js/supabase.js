6d93733783e1dd0fd17680c7e791dcbb3a172800
*Drперc* <stepchik.tringer@gmail.com>
2026-04-05 14:45:52 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -3,11 +3,3 @@ const SUPABASE_KEY = 'sb_publishable_GRAiUwNcIIDaBwl8Ux1TuA_1JhaIv6h';
 
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-window.createRequest = createRequest;
-window.fetchRequests = fetchRequests;
-window.submitBeat = submitBeat;
-window.showPage = showPage;
-window.toggleLike = toggleLike;
-window.fetchPosts = fetchPosts;
-window.handleAuth = handleAuth;
-window.logout = logout;
