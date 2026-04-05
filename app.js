ad850697bbbf75ebbc3af461f28cd1548a6948e9
*Drперc* <stepchik.tringer@gmail.com>
2026-04-05 14:17:46 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,4 +5,13 @@ window.onload = () => {
 
     if (activePage === 'feed') fetchPosts();
     if (activePage === 'requests') fetchRequests();
+
+    window.createRequest = createRequest;
+window.fetchRequests = fetchRequests;
+window.submitBeat = submitBeat;
+window.showPage = showPage;
+window.toggleLike = toggleLike;
+window.fetchPosts = fetchPosts;
+window.handleAuth = handleAuth;
+window.logout = logout;
 };
